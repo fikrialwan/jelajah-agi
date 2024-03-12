d414facf71e218d4bba290eb96365568b345a964
fikrialwan <fikriar93@gmail.com>
2024-03-12 14:07:57 +0700

    feat: add font and update metadata

---
+++ src/lib/styles/font.ts
@@ -0,0 +1,7 @@
+import { Plus_Jakarta_Sans } from "next/font/google";
+
+export const fontJakarta = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-jakarta",
+});
