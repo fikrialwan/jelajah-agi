d0f72f7eaa7fa0d8d40e748e66525f1d75c03d1d
Riko Chair Nugroho <riko.chairnugroho@ai.astra.co.id>
2024-03-19 08:04:37 +0700

    feat: add tabs

--- src/lib/components/features/leaderboard/index.tsx
+++ src/lib/components/features/leaderboard/index.tsx
@@ -1,10 +1,21 @@
+"use client";
+
+import { useState } from "react";
 import { Card, CardContent } from "../../ui/card";
 
-const data = [
+const dataTeam = [
   { teamName: "Team 1", score: "100" },
   { teamName: "Team 2", score: "100" },
   { teamName: "Team 3", score: "100" },
   { teamName: "Team 4", score: "100" },
+];
+const dataLomba = [
+  {
+    name: "All",
+  },
+  {
+    name: "Jenazah",
+  },
 ];
 
 const CardBoard = (props: {
@@ -14,33 +25,57 @@ const CardBoard = (props: {
 }) => {
   const { teamName, score, index } = props;
   return (
-    <Card className="my-3">
+    <Card className="my-3 border border-primary shadow-md">
       <CardContent
-        className={`flex justify-between items-center p-5 rounded-sm font-bold
-      ${
-        index === 0
-          ? "bg-[#d4af37] bg-gradi"
-          : index === 1
-          ? "bg-[#c0c0c0]"
-          : index === 2
-          ? "bg-[#CD7F32]"
-          : ""
-      }`}
+        className={`flex justify-between items-center p-5 rounded-sm font-bold`}
       >
-        <span>{teamName}</span>
-        <span>{score}</span>
+        <span>
+          <span
+            className={`mr-5 border p-3 rounded-full ${
+              index === 0
+                ? "bg-[#d4af37]"
+                : index === 1
+                ? "bg-[#c0c0c0]"
+                : index === 2
+                ? "bg-[#CD7F32]"
+                : ""
+            }`}
+          >
+            {index + 1}
+          </span>
+          {teamName}
+        </span>
+        <span>{score} points</span>
       </CardContent>
     </Card>
   );
 };
 
 const LeaderboardList = () => {
+  const [currentTab, setCurrentTab] = useState<number>(0);
   return (
     <div className="w-full">
       <div className="text-center mb-5">
         <h1 className="font-semibold text-2xl">Leaderboard</h1>
       </div>
-      {data.map((item, index) => {
+      <div className="border-b-2 flex gap-2 mb-4">
+        {dataLomba.map((data, index) => {
+          return (
+            <div
+              className={`p-2 cursor-pointer ${
+                currentTab === index ? "font-bold" : ""
+              }`}
+              key={index}
+              onClick={() => {
+                setCurrentTab(index);
+              }}
+            >
+              {data.name}
+            </div>
+          );
+        })}
+      </div>
+      {dataTeam.map((item, index) => {
         return (
           <CardBoard
             score={item.score}
--- src/lib/components/pages/index.tsx
+++ src/lib/components/pages/index.tsx
@@ -3,7 +3,7 @@ import LeaderboardList from "../features/leaderboard";
 
 export default function Home() {
   return (
-    <main className="container w-full h-screen py-8">
+    <main className="container w-full h-screen py-8 border-x">
       {/* <h1 className="text-primary font-semibold text-2xl">Jelajah AGI</h1> */}
       <LeaderboardList />
     </main>
