b1b8b555e0840c1bb38c7d9ff2fe577f37f52af6
Riko Chair Nugroho <riko.chair@gmail.com>
2024-03-25 01:22:45 +0700

    feat: dialog input number of participants

--- src/lib/components/pages/judge/index.tsx
+++ src/lib/components/pages/judge/index.tsx
@@ -1,3 +1,35 @@
+"use client";
+
+import { useState } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../../ui/dialog";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../../ui/form";
+import { Input } from "../../ui/input";
+import { Label } from "../../ui/label";
+import { Button } from "../../ui/button";
+
+interface IListTeam {
+  id: number;
+  name: string;
+  isValidate: boolean;
+}
+
 const CardTeam = (props: { name: string; isValidate: boolean }) => {
   const { name, isValidate } = props;
   return (
@@ -14,13 +46,15 @@ const CardTeam = (props: { name: string; isValidate: boolean }) => {
   );
 };
 
-const data = {
+const data: { teams: IListTeam[] } = {
   teams: [
     {
+      id: 1,
       name: "Team 1",
       isValidate: true,
     },
     {
+      id: 2,
       name: "Team 2",
       isValidate: false,
     },
@@ -28,16 +62,66 @@ const data = {
 };
 
 const ListTeamBooth = () => {
+  const [dialogTeam, setDialogTeam] = useState<IListTeam | null>(null);
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
+
+  const handleSubmitValidate = (e: any) => {
+    e.preventDefault();
+    setDialogTeam(null);
+    setOpenDialog(false);
+    console.log("submit");
+  };
   return (
-    <div>
-      {data.teams.map((team, index) => {
-        return (
-          <div key={index} className="mb-5">
-            <CardTeam name={team.name} isValidate={team.isValidate} />
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className="flex w-full flex-col">
+        {data.teams.map((team, index) => {
+          return (
+            <button
+              key={index}
+              className="mb-5"
+              onClick={() => {
+                setDialogTeam(team);
+                setOpenDialog(true);
+              }}
+            >
+              <CardTeam name={team.name} isValidate={team.isValidate} />
+            </button>
+          );
+        })}
+      </div>
+      <Dialog
+        open={openDialog}
+        onOpenChange={(e) => {
+          if (!e) {
+            setDialogTeam(null);
+            setOpenDialog(false);
+          }
+        }}
+      >
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Validasi {dialogTeam?.name}</DialogTitle>
+          </DialogHeader>
+          <form onSubmit={handleSubmitValidate}>
+            <div className="my-5 grid flex-1 gap-2">
+              <Label htmlFor="numberOfParticipants">
+                Number of Participants
+              </Label>
+              <Input
+                type="number"
+                id="numberOfParticipants"
+                placeholder="Input Number of Participants"
+              />
+            </div>
+            <DialogFooter>
+              <Button type="submit" variant="default">
+                Validate
+              </Button>
+            </DialogFooter>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
