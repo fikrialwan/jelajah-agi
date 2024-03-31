515ff548b46a688c3614b8c747c4868923adfd77
fikrialwan <fikriar93@gmail.com>
2024-03-31 15:33:00 +0700

    feat: ui list booth page

--- src/lib/components/pages/ruler/booth.tsx
+++ src/lib/components/pages/ruler/booth.tsx
@@ -1,5 +1,22 @@
+import { Plus } from "lucide-react";
+import { Button } from "~/lib/components/ui/button";
+import CardBooth from "../../features/ruler/booth/card";
+import { BoothData } from "~/data/booth-dummy";
+
 function Booth() {
-  return <div>RulerBooth</div>;
+  return (
+    <div className="flex flex-col gap-4 py-3 px-5">
+      <h1 className="text-3xl font-semibold text-center">List booth</h1>
+      <Button variant="default" className="w-24 self-end">
+        <Plus /> Add
+      </Button>
+      <ul className="flex flex-col gap-3">
+        {BoothData.map(({ title, img, pic }, index: number) => (
+          <CardBooth pic={pic} key={index} title={title} img={img} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Booth;
