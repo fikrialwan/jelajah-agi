db2cc3deaa6cbc1fa9006425476283e5e65dfffd
fikrialwan <fikriar93@gmail.com>
2024-04-17 05:39:32 +0700

    feat: add user component

--- src/lib/components/pages/ruler/user.tsx
+++ src/lib/components/pages/ruler/user.tsx
@@ -1,11 +1,12 @@
 import { UserData } from "~/data/user-dummy";
 import CardUser from "../../features/ruler/user/card";
+import AddUser from "../../features/ruler/user/add";
 
 export default function User() {
   return (
     <div className="flex flex-col gap-4 py-3 px-5">
       <h1 className="text-3xl font-semibold text-center">List user</h1>
-      {/* <BoothAdd /> */}
+      <AddUser />
       <ul className="flex flex-col gap-3">
         {UserData.map(({ name, booth }, index: number) => (
           <CardUser key={index} name={name} booth={booth} />
