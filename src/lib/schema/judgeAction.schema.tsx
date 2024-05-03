0b6a47c3a1898ea6387d7bc0b5aadd6024cd76ed
fikrialwan <fikriar93@gmail.com>
2024-05-03 07:57:44 +0700

    fix: message error min 1 value

--- src/lib/schema/judgeAction.schema.tsx
+++ src/lib/schema/judgeAction.schema.tsx
@@ -3,11 +3,11 @@ import { z } from "zod";
 export const validateParticipantFormSchema = z.object({
   numberOfParticipants: z
     .number({ required_error: "This field is required" })
-    .min(1, { message: "This field is required" }),
+    .min(1, { message: "The value min is 1" }),
 });
 
 export const inputScoreFormSchema = z.object({
   score: z
     .number({ required_error: "This field is required" })
-    .min(1, { message: "This field is required" }),
+    .min(1, { message: "The value min is 1" }),
 });
