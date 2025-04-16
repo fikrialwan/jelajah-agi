67c849172e3b585dbaed61b7e3a899c5eee4768d
fikrialwan <fikriar93@gmail.com>
2025-04-16 07:10:48 +0700

    fix: scanner ui

--- src/lib/components/features/ruler/participants/upload-result.tsx
+++ src/lib/components/features/ruler/participants/upload-result.tsx
@@ -140,59 +140,58 @@ export default function UploadResult({
   };
 
   useEffect(() => {
-    if (open) {
-      const html5QrCode = new Html5Qrcode("scan-qr-reader");
-      const qrCodeSuccessCallback = (decodedText: any) => {
-        // decodedText = text hasil scan
-        // decodedResult = {result text, decoderName, format dll}
+    const html5QrCode = new Html5Qrcode("scan-qr-reader");
+    const qrCodeSuccessCallback = (decodedText: any) => {
+      // decodedText = text hasil scan
+      // decodedResult = {result text, decoderName, format dll}
 
-        html5QrCode.stop().then(() => {
-          // Show success message
-          setShowQRScanner(false);
-          if (checkCountdownValid(endCountdown)) {
-            if (decodedText === currentBoothDetail.slugEnd) {
-              handleSubmit();
-            } else {
-              toast({
-                variant: "destructive",
-                title: "Can't scan QR Code. Please scan the correct QR Code.",
-              });
-              setOpen(false);
-            }
+      html5QrCode.stop().then(() => {
+        // Show success message
+        setShowQRScanner(false);
+        if (checkCountdownValid(endCountdown)) {
+          if (decodedText === currentBoothDetail.slugEnd) {
+            handleSubmit();
           } else {
             toast({
               variant: "destructive",
-              title: "Uh oh! Failed.",
-              description: "Waktu telah habis.",
+              title: "Can't scan QR Code. Please scan the correct QR Code.",
             });
             setOpen(false);
           }
-        });
-        /* handle success */
-      };
-      const qrCodeErrorCallback = (error: any) => {
-        console.log(error);
-        /* handle success */
-      };
+        } else {
+          toast({
+            variant: "destructive",
+            title: "Uh oh! Failed.",
+            description: "Waktu telah habis.",
+          });
+          setOpen(false);
+        }
+      });
+      /* handle success */
+    };
+    const qrCodeErrorCallback = (error: any) => {
+      console.log(error);
+      /* handle success */
+    };
 
-      // Check if when scan is the time is still available !!
+    // Check if when scan is the time is still available !!
 
-      if (showQRScanner) {
-        html5QrCode.start(
-          { facingMode: "environment" },
-          { fps: 10, qrbox: { height: 250, width: 250 } },
-          qrCodeSuccessCallback,
-          qrCodeErrorCallback,
-        );
-      } else {
-        html5QrCode.clear();
-      }
+    if (showQRScanner) {
+      html5QrCode.start(
+        { facingMode: "environment" },
+        { fps: 10, qrbox: { height: 250, width: 250 } },
+        qrCodeSuccessCallback,
+        qrCodeErrorCallback,
+      );
+    } else {
+      html5QrCode.clear();
     }
+
     // cleanup function when component will unmount
     return () => {
       //   html5QrCode.clear();
     };
-  }, [showQRScanner, open]);
+  }, [showQRScanner]);
 
   return (
     <>
@@ -204,66 +203,64 @@ export default function UploadResult({
       >
         Upload hasil
       </Button>
+      <div
+        id="scan-qr-reader"
+        className={`w-screen h-screen !fixed top-0 left-0 ${showQRScanner ? "block" : "hidden"} z-50`}
+      />
       <Dialog open={open}>
         <DialogContent>
-          {showQRScanner ? (
-            <div
-              id="scan-qr-reader"
-              className="w-full h-[300px] flex items-center justify-center"
-            />
-          ) : (
-            <DialogHeader>
-              <DialogTitle className="text-center">Upload hasil</DialogTitle>
-              <form className="flex flex-col gap-2">
-                {typeResult === "link" && (
-                  <fieldset className="flex flex-col items-start">
-                    <label>Link</label>
-                    <Input
-                      onChange={(e) => setResult(e.target.value)}
-                      placeholder="ex. https://www.instagram.com/astragemaislami/"
-                      name="link"
-                    />
-                  </fieldset>
-                )}
+          <DialogHeader>
+            <DialogTitle className="text-center">Upload hasil</DialogTitle>
+            <form className="flex flex-col gap-2">
+              {typeResult === "link" && (
+                <fieldset className="flex flex-col items-start">
+                  <label>Link</label>
+                  <Input
+                    onChange={(e) => setResult(e.target.value)}
+                    placeholder="ex. https://www.instagram.com/astragemaislami/"
+                    name="link"
+                  />
+                </fieldset>
+              )}
 
-                {typeResult === "file" && (
-                  <fieldset className="flex flex-col items-start">
-                    <label>Image</label>
-                    <Input
-                      name="img"
-                      type="file"
-                      accept="image/*"
-                      onChange={(e) => {
-                        if (e.target.files) {
-                          setFile(e.target.files[0]);
-                        } else {
-                          setFile(null);
-                        }
-                      }}
-                    />
-                  </fieldset>
-                )}
-                <Button
-                  type="button"
-                  className="mt-2"
-                  variant="default"
-                  onClick={() => {
-                    setShowQRScanner(true);
-                  }}
-                >
-                  Save
-                </Button>
-                <Button
-                  type="button"
-                  className="mt-2"
-                  variant="secondary"
-                  onClick={() => setOpen(false)}
-                >
-                  Close
-                </Button>
-              </form>
-            </DialogHeader>
-          )}
+              {typeResult === "file" && (
+                <fieldset className="flex flex-col items-start">
+                  <label>Image</label>
+                  <Input
+                    name="img"
+                    type="file"
+                    accept="image/*"
+                    onChange={(e) => {
+                      if (e.target.files) {
+                        setFile(e.target.files[0]);
+                      } else {
+                        setFile(null);
+                      }
+                    }}
+                  />
+                </fieldset>
+              )}
+              <Button
+                type="button"
+                className="mt-2"
+                variant="default"
+                onClick={() => {
+                  setShowQRScanner(true);
+                  setOpen(false);
+                }}
+              >
+                Save
+              </Button>
+              <Button
+                type="button"
+                className="mt-2"
+                variant="secondary"
+                onClick={() => setOpen(false)}
+              >
+                Close
+              </Button>
+            </form>
+          </DialogHeader>
         </DialogContent>
       </Dialog>
     </>
